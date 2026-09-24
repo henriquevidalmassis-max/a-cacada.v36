c231d2caf6e8d07af62eafc5c8320426ab14c833
henriquevidalmassis-max <henrique.vidalmassis@gmail.com>
2026-09-24 17:39:44 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE='a-cacada-v20';
+const ASSETS=['./','./index.html','./background.png','./manifest.json','./config.js'];
+
+self.addEventListener('install',e=>
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  )
+);
+
+self.addEventListener('activate',e=>
+  e.waitUntil(
+    caches.keys()
+      .then(ks=>Promise.all(
+        ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  )
+);
+
+self.addEventListener('fetch',e=>
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+);
