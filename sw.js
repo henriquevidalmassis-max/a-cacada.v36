3a281e77c0a6a3b4f4067ba985fc63b41c701423
henriquevidalmassis-max <henrique.vidalmassis@gmail.com>
2026-09-24 17:51:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,36 @@
-const CACHE='a-cacada-v20';
-const ASSETS=['./','./index.html','./background.png','./manifest.json','./config.js'];
+const CACHE='a-cacada-v37';
 
-self.addEventListener('install',e=>
-  e.waitUntil(
+const ASSETS=[
+  './',
+  './index.html',
+  './background.png',
+  './manifest.json',
+  './config.js'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
     caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
+      .then(cache=>cache.addAll(ASSETS))
       .then(()=>self.skipWaiting())
-  )
-);
+  );
+});
 
-self.addEventListener('activate',e=>
-  e.waitUntil(
+self.addEventListener('activate',event=>{
+  event.waitUntil(
     caches.keys()
-      .then(ks=>Promise.all(
-        ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      .then(keys=>Promise.all(
+        keys
+          .filter(key=>key!==CACHE)
+          .map(key=>caches.delete(key))
       ))
       .then(()=>self.clients.claim())
-  )
-);
+  );
+});
 
-self.addEventListener('fetch',e=>
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  )
-);
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request)
+      .then(response=>response || fetch(event.request))
+  );
+});
