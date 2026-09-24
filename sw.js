9e90a3b790883437a5604c8ac9132a56a3332b4f
henriquevidalmassis-max <henrique.vidalmassis@gmail.com>
2026-09-24 19:15:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,24 @@
-const CACHE = 'a-cacada-v37';
+const CACHE='a-cacada-v39';
+const ASSETS=['./','./index.html','./background.png','./manifest.json','./config.js'];
 
-const ASSETS = [
-  './',
-  './index.html',
-  './background.png',
-  './manifest.json',
-  './config.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install',e=>
+  e.waitUntil(
     caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  )
+);
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate',e=>
+  e.waitUntil(
     caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE)
-            .map(key => caches.delete(key))
-        )
-      )
-      .then(() => self.clients.claim())
-  );
-});
+      .then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  )
+);
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('fetch',e=>
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+);
